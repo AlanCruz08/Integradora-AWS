a958064ae6967240863d3e939c750423e86e842c
Soysho <issacgar98@gmail.com>
2023-11-16 02:15:07 -0600

    Se agrego un crud funcional, se hicieron cambios en las rutas para mongoDBCompass

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\RegistroController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(RegistroController::class)->group(function(){
+    Route::get('registros', [RegistroController::class, 'index']);
+    Route::post('registros', [RegistroController::class, 'store']);
+    Route::put('registros/{registro}', [RegistroController::class, 'update']);
+    Route::delete('registros/{registro}', [RegistroController::class, 'destroy']);
 });
