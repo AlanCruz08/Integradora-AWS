043d666da556eb488a7f9d4710ef17e5a17bdfd6
Soysho <issacgar98@gmail.com>
2023-11-29 21:19:58 -0600

    cambio en vista y controlador

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -1,2 +1,13 @@
-<h1>Correo Verificacion</h1>
-<h2>Este es tu codigo de verificacion {{ $number }}</h2>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Correo de verificación</title>
+</head>
+<body style="background-color: #8CD2D9; color: #000; font-family: Arial, Helvetica, sans-serif; text-align: center;">
+  <div class="container" style="width: 50%; margin: 0 auto;">
+    <h1>Codigo de verificación</h1>
+    <h2>Codigo : {{ $number }}</h2>
+  </div>
+</body>
+</html>
