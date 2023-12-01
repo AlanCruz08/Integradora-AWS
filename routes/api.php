f05aad0e0541d362649c665f7dcda6e348d79cd5
Alan <alansasuke0@gmail.com>
2023-11-30 19:57:27 -0600

    eliminacion comment en api

--- routes/api.php
+++ routes/api.php
@@ -7,17 +7,6 @@ use App\Http\Controllers\Api\InfoGeneralController;
 use App\Http\Controllers\Api\SensorController;
 use App\Http\Controllers\LoginController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
