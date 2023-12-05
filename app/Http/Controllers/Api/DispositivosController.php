5dc82708758ab6a12a352a6033d125554f14a05d
Soysho <issacgar98@gmail.com>
2023-12-05 14:36:00 -0600

    se modifico un controlador

--- app/Http/Controllers/Api/DispositivosController.php
+++ app/Http/Controllers/Api/DispositivosController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\Dispositivos;
 use Symfony\Component\HttpFoundation\Response;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;   
 
 
